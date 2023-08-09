bf47466be3e286a42075a3f36e501a25e4b1d70c
Yulia <meshokyulia0002@gmail.com>
2023-08-09 19:24:53 +0300

    hw_2

---
+++ hw2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99)
+{
+    Console.WriteLine(" Третья цифра " + (number%10)%10);
+}
+else if (number<99)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
