2694a8bb3e2d690710d48920dc2989733da35eba
Yulia <meshokyulia0002@gmail.com>
2023-08-09 19:28:17 +0300

    hw_3

---
+++ hw3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Этот день выходгой : да " );
+}
+else if (number<1 || number>7)
+{
+    Console.WriteLine("Это вообще не день недели");
+}
+else Console.WriteLine("Этот день не выходной : нет" );
